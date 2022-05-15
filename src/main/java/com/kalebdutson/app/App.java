5de360efb8c93321d8b1621b43f987c25aca3bd3
Kaleb Dutson <kalebdutson11@gmail.com>
2022-05-14 19:59:00 -0600

    Added arguments to initialize NewMacroWindow

--- src/main/java/com/kalebdutson/app/App.java
+++ src/main/java/com/kalebdutson/app/App.java
@@ -5,8 +5,15 @@ import javax.swing.*;
 public class App
 {
     public static void main( String[] args ) {
+        Config config = new Config();
         // To run jnativehook, use the invokeLater method since jnativehook doesn't
         //  operate on the event dispatch thread
-        SwingUtilities.invokeLater(NewMacroWindow::new);
+        SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        new NewMacroWindow(config);
+                    }
+                }
+        );
     }
 }
