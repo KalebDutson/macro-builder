b9e5f06ae649439329e74486392195a2fc7393ac
Kaleb Dutson <kalebdutson11@gmail.com>
2022-05-15 21:12:22 -0600

    Built static functions to register nativehooks for use with all windows that need to track keyboard and mouse input

--- src/main/java/com/kalebdutson/app/App.java
+++ src/main/java/com/kalebdutson/app/App.java
@@ -1,5 +1,9 @@
 package com.kalebdutson.app;
 
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
 import javax.swing.*;
 
 public class App
@@ -15,5 +19,29 @@ public class App
                     }
                 }
         );
+    }
+    public static void registerHook(NativeKeyListener listener, String classname){
+        try{
+            GlobalScreen.registerNativeHook();
+        }
+        catch(NativeHookException ex){
+            System.err.printf("There was a problem registering the hook for class: %s%n", classname);
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        GlobalScreen.addNativeKeyListener(listener);
+    }
+
+    public static void unregisterHook(String classname){
+        // Clean up the native hook
+        try {
+            GlobalScreen.unregisterNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.printf("Encountered error unregistering Nativehook for class: %s%n", classname);
+            throw new RuntimeException(ex);
+        }
+        System.runFinalization();
+        System.exit(0);
     }
 }
