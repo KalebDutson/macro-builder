4987ff39709415d79c55b322f317e0e67208a2dc
Kaleb Dutson <kalebdutson11@gmail.com>
2022-06-07 20:47:40 -0600

    Added font variables for consistency

--- src/main/java/com/kalebdutson/app/App.java
+++ src/main/java/com/kalebdutson/app/App.java
@@ -5,9 +5,12 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class App
 {
+    static final Font FONT_A_PLAIN = new Font("Courier", Font.PLAIN, 10 );
+    static final Font FONT_A_BOLD = new Font("Courier", Font.BOLD, 12);
     public static void main( String[] args ) {
         Config config = new Config();
         // To run jnativehook, use the invokeLater method since jnativehook doesn't
