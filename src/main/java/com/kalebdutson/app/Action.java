903a0e90340fdd3e2a9ed553fc0430c4f2020312
Kaleb Dutson <kalebdutson11@gmail.com>
2022-06-10 00:22:23 -0600

    Tweaked Action class to be abstract since it won't be instantiated by itself.

--- src/main/java/com/kalebdutson/app/Action.java
+++ src/main/java/com/kalebdutson/app/Action.java
@@ -5,13 +5,11 @@ import java.awt.*;
 /**
  * Input action from the keyboard or mouse
  */
-public class Action {
-    private float positionX;
-    private float positionY;
+public abstract class Action {
     private float executionStartMillis;
 
     /**
-     * Creates an Action instance with a specified execution time.
+     * Action with a specified execution time.
      * @param millis After macro execution, the amount of time until this action is executed.
      */
     public Action(float millis){
@@ -21,61 +19,8 @@ public class Action {
         this.executionStartMillis = millis;
     }
 
-    /**
-     * Creates an Action instance with a specific screen location.
-     * @param x X position of the Action on the screen.
-     * @param y Y position of the Action on the screen.
-     */
-    public Action(float x, float y){
-        if(x < 0) {
-            throw new IllegalArgumentException("Must provide a positive value for \"x\"");
-        }
-        if(y < 0){
-            throw new IllegalArgumentException("Must provide a positive value for \"y\"");
-        }
-        this.positionX = x;
-        this.positionY = y;
-    }
-
-    /**
-     * Creates an Action instance with a specific screen location and execution time.
-     * @param x X position of the Action on the screen.
-     * @param y Y position of the Action on the screen.
-     * @param millis After macro execution, the amount of time until this action is executed.
-     */
-    public Action(float x, float y, float millis){
-        if(millis < 0){
-            throw new IllegalArgumentException("Must provide a positive value for \"millis\"");
-        }
-        if(x < 0) {
-            throw new IllegalArgumentException("Must provide a positive value for \"x\"");
-        }
-        if(y < 0){
-            throw new IllegalArgumentException("Must provide a positive value for \"y\"");
-        }
-        this.positionX = x;
-        this.positionY = y;
-        this.executionStartMillis = millis;
-    }
-
-    public float getPositionX() {
-        return positionX;
-    }
-
-    public float getPositionY() {
-        return positionY;
-    }
-
     public float getExecutionStartMillis() {
         return executionStartMillis;
-    }
-
-    public void setPositionX(float x){
-        this.positionX = x;
-    }
-
-    public void setPositionY(float y){
-        this.positionY = y;
     }
 
     public void setExecutionStartMillis(float millis){
