0d65c1bafb993b5cde6729ad8be0167f5616bc49
Kaleb Dutson <kalebdutson11@gmail.com>
2022-08-22 17:22:49 -0600

    Initiallized Parser class that will parse data from the Macro class into a JSON format for saving and loading Macros.

---
+++ src/main/java/com/kalebdutson/app/Parser.java
@@ -0,0 +1,20 @@
+package com.kalebdutson.app;
+
+import com.kalebdutson.app.models.Macro;
+
+import java.io.IOException;
+
+/**
+ * Parses Macro objects to/from JSON format for saving/loading Macros into the MacroBuilder.
+ */
+public class Parser {
+    public static String toJSON(Macro m){
+        return "";
+    }
+
+    public static Macro toMacro(String filename) throws IOException {
+        Macro m = new Macro();
+
+        return m;
+    }
+}
