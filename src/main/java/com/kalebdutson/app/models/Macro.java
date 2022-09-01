61685e21165049b2c441707f8c6949475be041fe
Kaleb Dutson <kalebdutson11@gmail.com>
2022-08-31 19:11:51 -0600

    Added Macro class implementation

--- src/main/java/com/kalebdutson/app/models/Macro.java
+++ src/main/java/com/kalebdutson/app/models/Macro.java
@@ -1,10 +1,41 @@
 package com.kalebdutson.app.models;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
+import java.util.ArrayList;
 
 
 public class Macro {
 
+    private int iterations;
+    private ArrayList<NativeMouseEvent> mouseEvents;
+    private ArrayList<NativeKeyEvent> keyEvents;
 
-
+    // Getters
+    public int getIterations(){
+        return this.iterations;
+    }
+    public ArrayList<NativeKeyEvent> getKeyEvents(){
+        return this.keyEvents;
+    }
+    public ArrayList<NativeMouseEvent> getMouseEvents(){
+        return this.mouseEvents;
+    }
+    // Setters
+    public void setIterations(int i){
+        this.iterations = i;
+    }
+    public void setKeyEvents(ArrayList<NativeKeyEvent> keyEvents){
+        this.keyEvents = keyEvents;
+    }
+    public void setMouseEvents(ArrayList<NativeMouseEvent> mouseEvents){
+        this.mouseEvents = mouseEvents;
+    }
+    // Add items
+    public void addKeyEvent(NativeKeyEvent keyEvent){
+        this.keyEvents.add(keyEvent);
+    }
+    public void addMouseEvent(NativeMouseEvent mouseEvent){
+        this.mouseEvents.add(mouseEvent);
+    }
 
 }
