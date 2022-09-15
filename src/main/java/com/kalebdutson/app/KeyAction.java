5384ea11addd4b70fd709e43a669eade3905f659
Kaleb Dutson <kalebdutson11@gmail.com>
2022-09-15 11:05:00 -0600

    Minor syntax refactoring. Overloaded addKeyAction and addMouseAction methods to accept KeyAction or NativeKeyEvent objects.'

--- src/main/java/com/kalebdutson/app/KeyAction.java
+++ src/main/java/com/kalebdutson/app/KeyAction.java
@@ -3,20 +3,20 @@ package com.kalebdutson.app;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 
 public class KeyAction extends Action{
-    private NativeKeyEvent ne;
+    private NativeKeyEvent ke;
 
     /**
      * Create a KeyAction instance for a specific keystroke at a specific time
      * @param when Time when macro is executed in milliseconds
-     * @param ne NativeKeyEvent for key press info and modifiers
+     * @param ke NativeKeyEvent for key press info and modifiers
      */
-    public KeyAction(long when, NativeKeyEvent ne){
+    public KeyAction(long when, NativeKeyEvent ke){
         super(when);
-        this.ne = ne;
+        this.ke = ke;
     }
 
     public int getKeyCode() {
-        return this.ne.getKeyCode();
+        return this.ke.getKeyCode();
     }
 
     // TODO: Implement other methods
